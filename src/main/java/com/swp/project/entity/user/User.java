fbd8152181026d37816fad7b338dc1ed54eb1e36
Luu Quang Truong <truongcuc05@gmail.com>
2025-09-15 22:06:12 +0700

    set @Column parameter for entities (update)

--- src/main/java/com/swp/project/entity/user/User.java
+++ src/main/java/com/swp/project/entity/user/User.java
@@ -22,7 +22,7 @@ public abstract class User implements OAuth2User, UserDetails {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true, length = 50)
+    @Column(nullable = false, unique = true)
     private String email;
 
     @Column(nullable = false, length = 50)
