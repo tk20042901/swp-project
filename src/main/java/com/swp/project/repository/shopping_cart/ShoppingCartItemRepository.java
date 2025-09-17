1eb151a1dd0ae52a83f2a559ad8d7c1150d6a7c0
Nghia <nghianguyentin99@gmail.com>
2025-09-17 09:24:50 +0700

    view, update, delete cart_item

---
+++ src/main/java/com/swp/project/repository/shopping_cart/ShoppingCartItemRepository.java
@@ -0,0 +1,18 @@
+package com.swp.project.repository.shopping_cart;
+
+import com.swp.project.entity.shopping_cart.ShoppingCartItem;
+import com.swp.project.entity.shopping_cart.ShoppingCartItemId;
+import com.swp.project.entity.user.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ShoppingCartItemRepository extends JpaRepository<ShoppingCartItem, ShoppingCartItemId> {
+    List<ShoppingCartItem> findByCustomer(Customer customer);
+
+    ShoppingCartItem findShoppingCartItemById(ShoppingCartItemId id);
+
+
+}
--- src/main/java/com/swp/project/repository/user/CustomerRepository.java
+++ src/main/java/com/swp/project/repository/user/CustomerRepository.java
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Repository;
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     boolean existsByEmail(String email);
     Customer getByEmail(String email);
+
+    Customer getCustomerByEmail(String email);
 }
