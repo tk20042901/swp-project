f96da52c8979f447479b0ff3d1b5d166f6e8af1f
Nghia <nghianguyentin99@gmail.com>
2025-09-18 20:21:26 +0700

    update shopping_cart

--- src/main/java/com/swp/project/service/product/ProductService.java
+++ src/main/java/com/swp/project/service/product/ProductService.java
@@ -1,5 +1,7 @@
 package com.swp.project.service.product;
 
+import com.swp.project.entity.product.Product;
+import com.swp.project.entity.product.ProductBatch;
 import com.swp.project.repository.product.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,4 +12,11 @@ import org.springframework.stereotype.Service;
 public class ProductService {
     private final ProductRepository productRepository;
 
+    public int getAvailableQuantity(Product product) {
+        int total = 0;
+        for (ProductBatch batch : product.getProductBatches()) {
+            total += batch.getQuantity();
+        }
+        return total;
+    }
 }
