b1b234d0058b5fcbbfd0185448a64738a600bff3
hieunhangia <hieuvippro531@gmail.com>
2025-09-20 13:07:47 +0700

    fix redundant methods

--- src/main/java/com/swp/project/repository/order/OrderRepository.java
+++ src/main/java/com/swp/project/repository/order/OrderRepository.java
@@ -1,11 +1,14 @@
 package com.swp.project.repository.order;
 
 import com.swp.project.entity.order.Order;
-import com.swp.project.entity.order.OrderStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface OrderRepository extends JpaRepository<Order,Long> {
-    Page<Order> getByOrderStatus(OrderStatus orderStatus, Pageable pageable);
+    Page<Order> searchByCustomer_EmailContainsAndOrderDateBetween(String customer_email, LocalDateTime toDate, LocalDateTime fromDate, Pageable pageable);
+
+    Page<Order> searchByOrderStatus_IdAndCustomer_EmailContainsAndOrderDateBetween(Long statusId,String customer_email,LocalDateTime toDate, LocalDateTime fromDate, Pageable pageable);
 }
