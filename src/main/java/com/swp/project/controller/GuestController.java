97adbb2a4b3c2f9776882d2c0e47b666cc2fe379
Luu Quang Truong <truongcuc05@gmail.com>
2025-09-22 21:07:09 +0700

    add product details page (update)

--- src/main/java/com/swp/project/controller/GuestController.java
+++ src/main/java/com/swp/project/controller/GuestController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.swp.project.dto.ViewProductDto;
