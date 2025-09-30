acd81a5ef9bc8320e1de1292024e4e2d6e2236ac
Nghia <nghianguyentin99@gmail.com>
2025-09-30 18:55:22 +0700

    create product list page for seller

--- src/main/java/com/swp/project/repository/product/ProductRepository.java
+++ src/main/java/com/swp/project/repository/product/ProductRepository.java
@@ -2,7 +2,9 @@ package com.swp.project.repository.product;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -17,4 +19,5 @@ public interface ProductRepository extends JpaRepository<Product,Long> {
 
     List<Product> getByName(String name);
     List<Product> findDistinctByCategoriesInAndIdNot(List<Category> categories, Long id, PageRequest of);
+
 }
--- src/main/java/com/swp/project/repository/user/SellerRepository.java
+++ src/main/java/com/swp/project/repository/user/SellerRepository.java
@@ -1,6 +1,9 @@
 package com.swp.project.repository.user;
 
+import com.swp.project.entity.product.Product;
 import com.swp.project.entity.user.Seller;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,5 +16,7 @@ public interface SellerRepository extends JpaRepository<Seller,Long> {
     Seller findByCid(String Cid);
 
     List<Seller> findByFullnameContainsAndCidContains(String name, String cId);
+
+
 
 }
