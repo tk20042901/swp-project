3a5a77539c6273aef37c04fb1b4a9c293b99ac1a
hieunhangia <hieuvippro531@gmail.com>
2025-10-02 14:15:56 +0700

    remove update product batch method

--- src/main/java/com/swp/project/service/product/ProductBatchService.java
+++ src/main/java/com/swp/project/service/product/ProductBatchService.java
@@ -16,23 +16,19 @@ public class ProductBatchService {
     private final ProductBatchRepository productBatchRepository;
     private final ApplicationEventPublisher eventPublisher;
 
-    public void addProductBatch(ProductBatch productBatch) {
-        productBatchRepository.save(productBatch);
-    }
 
     public ProductBatch getProductBatchById(Long id) {
         return productBatchRepository.findById(id).orElse(null);
     }
 
-    public void updateProductBatch(ProductBatch productBatch) {
-        productBatchRepository.save(productBatch);
-
-        eventPublisher.publishEvent(new ProductRelatedUpdateEvent
-                (getProductBatchById(productBatch.getId()).getProduct().getId()));
-    }
-
     public List<ProductBatch> getByProductId(Long productId) {
         return productBatchRepository.getByProduct_Id(productId);
+    }
+
+    public void addProductBatch(ProductBatch productBatch) {
+        productBatchRepository.save(productBatch);
+
+        eventPublisher.publishEvent(new ProductRelatedUpdateEvent(productBatch.getProduct().getId()));
     }
 
 }
