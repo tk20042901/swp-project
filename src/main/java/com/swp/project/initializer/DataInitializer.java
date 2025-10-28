0e666f1fd16d1e0fec24a6f8d8668bbb0aa2b152
hieunhangia <hieuvippro531@gmail.com>
2025-10-28 22:24:15 +0700

    remove account initializer

--- src/main/java/com/swp/project/initializer/DataInitializer.java
+++ src/main/java/com/swp/project/initializer/DataInitializer.java
@@ -1,14 +1,9 @@
 package com.swp.project.initializer;
 
-import com.swp.project.service.CustomerAiService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.swp.project.service.user.AdminService;
-import com.swp.project.service.user.CustomerService;
-import com.swp.project.service.user.ManagerService;
-import com.swp.project.service.user.SellerService;
-import com.swp.project.service.user.ShipperService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,19 +12,10 @@ import lombok.RequiredArgsConstructor;
 public class DataInitializer implements CommandLineRunner {
 
     private final AdminService adminService;
-    private final CustomerService customerService;
-    private final ManagerService managerService;
-    private final SellerService sellerService;
-    private final ShipperService shipperService;
-    private final CustomerAiService customerAiService;
 
     @Override
     public void run(String... args) {
         adminService.initAdmin();
-        managerService.initManager();
-        sellerService.initSeller();
-        shipperService.initShipper();
-        customerService.initCustomer();
 
 //        customerAiService.saveAllProductsFromDatabaseToVectorStore();
     }
