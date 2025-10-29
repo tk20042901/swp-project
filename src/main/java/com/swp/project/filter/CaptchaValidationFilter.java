3314436518576d749444f18c796594f2c963a72d
tk20042901 <tk20042901@gmail.com>
2025-10-29 17:21:27 +0700

    fix captcha

--- src/main/java/com/swp/project/filter/CaptchaValidationFilter.java
+++ src/main/java/com/swp/project/filter/CaptchaValidationFilter.java
@@ -50,7 +50,6 @@ public class CaptchaValidationFilter extends OncePerRequestFilter {
         params.add("response", recaptchaResponse);
         var body = new RestTemplate().postForObject("https://www.google.com/recaptcha/api/siteverify", params,
                 Map.class);
-        // return body != null && Boolean.TRUE.equals(body.get("success"));
-        return true;
+        return body != null && Boolean.TRUE.equals(body.get("success"));
     }
 }
