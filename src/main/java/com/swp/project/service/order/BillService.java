9b810b393e4406a7faa62a2657e8265cef1058f8
Luu Quang Truong <truongcuc05@gmail.com>
2025-10-29 23:44:26 +0700

    add sorting behaviours for role manager (bill management)

--- src/main/java/com/swp/project/service/order/BillService.java
+++ src/main/java/com/swp/project/service/order/BillService.java
@@ -18,24 +18,55 @@ import lombok.RequiredArgsConstructor;
 @Service
 public class BillService {
     private final BillRepository billRepository;
+    private final OrderService orderService;
 
-    public Page<Bill> getBills(int page, int size, String sortCriteria, int k) {
+    public Page<Bill> getBills(int page, int size, String searchQuery, String sortCriteria, int k, String sortCriteriaInPage) {
         Pageable pageable = PageRequest.of(page - 1, size);
         List<Bill> bills = billRepository.findAll()
         .stream()
         .sorted((o1, o2) -> {
             if (sortCriteria == null) return 0;
             return switch (sortCriteria) {
-                case "id" -> k * o1.getId().compareTo(o2.getId());
+                case "id" -> o1.getId().compareTo(o2.getId());
+                case "shopName" -> o1.getShopName().compareTo(o2.getShopName());
+                case "customer" -> o1.getOrder().getCustomer().getFullName().compareTo(o2.getOrder().getCustomer().getFullName());
                 case "paymentTime" ->
-                        k * o1.getOrder().getCurrentShippingStatus().getId().compareTo(o2.getOrder().getCurrentShippingStatus().getId());
+                        o1.getOrder().getCurrentShippingStatus().getId().compareTo(o2.getOrder().getCurrentShippingStatus().getId());
+                case "totalAmount" -> orderService.calculateTotalAmount(o1.getOrder())
+                        .compareTo(orderService.calculateTotalAmount(o2.getOrder()));
+                case "address" -> o1.getOrder().getAddressString().compareTo(o2.getOrder().getAddressString());
                 default -> 0;
             };
         })
         .toList();
+
+        if (searchQuery != null && !searchQuery.isEmpty()) {
+            String lowerCaseQuery = searchQuery.toLowerCase();
+            bills = bills.stream()
+                    .filter(bill -> bill.getOrder().getCustomer().getFullName().toLowerCase().contains(lowerCaseQuery))
+                    .toList();
+        }
+
         int start = Math.min((page - 1) * size, bills.size());
         int end = Math.min(start + size, bills.size());
         List<Bill> pagedBills = bills.subList(start, end);
+
+        pagedBills = pagedBills.stream()
+        .sorted((o1, o2) -> {
+            if (sortCriteriaInPage == null) return 0;
+            return switch (sortCriteriaInPage) {
+                case "id" -> k * o1.getId().compareTo(o2.getId());
+                case "shopName" -> k * o1.getShopName().compareTo(o2.getShopName());
+                case "customer" -> k * o1.getOrder().getCustomer().getFullName().compareTo(o2.getOrder().getCustomer().getFullName());
+                case "paymentTime" -> k * o1.getOrder().getCurrentShippingStatus().getId().compareTo(o2.getOrder().getCurrentShippingStatus().getId());
+                case "totalAmount" -> k * orderService.calculateTotalAmount(o1.getOrder())
+                        .compareTo(orderService.calculateTotalAmount(o2.getOrder()));
+                case "address" -> k * o1.getOrder().getAddressString().compareTo(o2.getOrder().getAddressString());
+                default -> 0;
+            };
+        })
+        .toList();
+
         return new PageImpl<>(pagedBills, pageable, bills.size());
     }
 
